fe646f57ff86fa3c450eaf6dbe5b83c1d196e0a5
Stelios Avgousti <56634058+stelios34S@users.noreply.github.com>
2025-02-12 11:29:32 +0100

    add joysick config

--- modules/hardwaremanager/hardwaremanager_inputs/joanjoystick.py
+++ modules/hardwaremanager/hardwaremanager_inputs/joanjoystick.py
@@ -153,22 +153,22 @@ class JoyStickSettings:
             settings_to_return.reverse_channel = 6
             settings_to_return.reverse_value = 10
         elif device == 'steeringwheel':
-            settings_to_return.min_steer = -1 * math.pi
-            settings_to_return.max_steer = 1 * math.pi
-            settings_to_return.degrees_of_freedom = 9
-            settings_to_return.gas_channel = 6
-            settings_to_return.use_separate_brake_channel = True
-            settings_to_return.brake_channel = 7
-            settings_to_return.first_steer_channel = 5
-            settings_to_return.use_double_steering_resolution = False
-            settings_to_return.second_steer_channel = -1
-            settings_to_return.hand_brake_channel = 0
-            settings_to_return.hand_brake_value = 24
-            settings_to_return.reverse_channel = 0
-            settings_to_return.reverse_value = 72
+            settings_to_return.min_steer = -0.5 * math.pi
+            settings_to_return.max_steer = 0.5 * math.pi
+            settings_to_return.degrees_of_freedom = 12
+            settings_to_return.gas_channel = 9
+            settings_to_return.use_separate_brake_channel = False
+            settings_to_return.brake_channel = -1
+            settings_to_return.first_steer_channel = 0
+            settings_to_return.use_double_steering_resolution = True
+            settings_to_return.second_steer_channel = 1
+            settings_to_return.hand_brake_channel = 10
+            settings_to_return.hand_brake_value = 2
+            settings_to_return.reverse_channel = 10
+            settings_to_return.reverse_value = 8
             settings_to_return.gear_channel = 1  # 2
-            settings_to_return.gear_up = 1  # 8
-            settings_to_return.gear_down = 2  # 64
+            settings_to_return.gear_up = 1   # 8
+            settings_to_return.gear_down = 2   # 64
         return settings_to_return
 
 
