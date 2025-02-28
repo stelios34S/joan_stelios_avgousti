1e8454429ce8f0bbfbeaf1e87c7791b4e432d14d
Stelios Avgousti <56634058+stelios34S@users.noreply.github.com>
2025-02-28 17:08:01 +0100

    push new location for trigger box

--- modules/carlainterface/carlainterface_agentclasses/my_vehicle.py
+++ modules/carlainterface/carlainterface_agentclasses/my_vehicle.py
@@ -157,7 +157,7 @@ class MyVehicleProcess:
         self.trigger_boxes = [
             {'location': carla.Location(x=-436.92488281, y=193.64867188, z=1.78496338), 'behavior': 'continue', 'target_speed': 30},
             # Stop box
-            {'location': carla.Location(x=-458.95835938, y=149.92051758, z=1.79914368), 'behavior': 'stop', 'target_speed': 0},
+            {'location': carla.Location(x=-462.91152344, y=117.94867188, z=1.79915588), 'behavior': 'stop', 'target_speed': 0},
             # Slight slowdown
             #{'location': carla.Location(x=350, y=250, z=0), 'behavior': 'slowdown', 'target_speed': 20},  # Slowdown box
         ]
